c650a581c69702403fa03f1aee8e8760a482fe83
davijaf <davijose@gmail.com>
2023-01-02 04:01:31 -0300

    second commit

---
+++ FirstConsoleApp/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FirstConsoleApp
+{
+    class Program
+    {
+        public static string title = "Treinamento Essencial C#";
+        public static int whichPart;
+        static void Main(string[] args)
+        {
+            whichPart = 2;
+            var message = string.Format("Welcome to {0} Part {1}", title, whichPart);
+            Console.WriteLine(message);
+            Console.WriteLine("Pressione a tecla Enter para sair.");
+            Console.ReadLine();
+        }
+    }
+}
